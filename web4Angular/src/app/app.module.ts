45cf65ad44d875b5b4322c6b84b17e87e1aaa834
OvchinnikovI17 <isofgh@gmail.com>
2023-11-28 04:29:02 +0300

    draft project Need to rewrite and add PrimeNG, rework home page

--- web4Angular/src/app/app.module.ts
+++ web4Angular/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { TableModule } from 'primeng/table';
+import { DropdownModule } from 'primeng/dropdown';
 import { AppComponent } from './app.component';
 import { StartComponent} from "./components/start/start.component";
 import { HeaderComponent} from "./components/header/header.component";
 import { LoginComponent} from "./components/login/login.component";
 import { RegistrationComponent} from "./components/registration/registration.component";
+import { MainComponent} from "./components/main/main.component";
 import {RouterModule, Routes} from "@angular/router";
 
 const appRoutes: Routes = [
   { path: '', component: StartComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
+  { path: 'main', component: MainComponent}
 ];
 
 @NgModule({
@@ -21,11 +25,14 @@ const appRoutes: Routes = [
     StartComponent,
     HeaderComponent,
     LoginComponent,
+    MainComponent,
     RegistrationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    TableModule,
+    DropdownModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
