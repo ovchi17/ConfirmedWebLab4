6bee97848dec1ec2d5d23c35eab676af36b21ee8
OvchinnikovI17 <isofgh@gmail.com>
2023-11-29 12:59:15 +0300

    angry

--- web4Angular/src/app/components/login/login.component.ts
+++ web4Angular/src/app/components/login/login.component.ts
@@ -6,4 +6,21 @@ import {Component, OnInit} from '@angular/core'
   styleUrl: './login.component.css'
 })
 
-export class LoginComponent {}
+export class LoginComponent {
+  loginValue: string = '';
+  passwordValue: string = '';
+  isTextVisible: boolean = false;
+  errorMessage: string = '';
+
+  checkInput() {
+    const containsRussian = /[а-яА-Я]/.test(this.loginValue) || /[а-яА-Я]/.test(this.passwordValue);
+    const loginLen = this.loginValue.length > 10;
+    this.isTextVisible = containsRussian || loginLen;
+    if (containsRussian){
+      this.errorMessage = 'Only english letters in login and password!';
+    }
+    if (loginLen){
+      this.errorMessage = 'Max login length is 10';
+    }
+  }
+}
--- web4Angular/src/app/components/registration/registration.component.ts
+++ web4Angular/src/app/components/registration/registration.component.ts
@@ -6,4 +6,21 @@ import {Component, OnInit} from '@angular/core'
   styleUrl: './registration.component.css'
 })
 
-export class RegistrationComponent {}
+export class RegistrationComponent {
+  loginValue: string = '';
+  passwordValue: string = '';
+  isTextVisible: boolean = false;
+  errorMessage: string = '';
+
+  checkInput() {
+    const containsRussian = /[а-яА-Я]/.test(this.loginValue) || /[а-яА-Я]/.test(this.passwordValue);
+    const loginLen = this.loginValue.length > 10;
+    this.isTextVisible = containsRussian || loginLen;
+    if (containsRussian){
+      this.errorMessage = 'Only english letters in login and password!';
+    }
+    if (loginLen){
+      this.errorMessage = 'Max login length is 10';
+    }
+  }
+}
